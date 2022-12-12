9a5b06692a0158661d049f0f69b2987542b1e31a
Savannah Sheppeard <caitlin_s_sheppeard@progressive.com>
2022-12-12 12:42:32 -0500

    BEFORE LUNCH

---
+++ backend/GiftingSolution/GiftingApi/Adapters/GiftingDataContext.cs
@@ -0,0 +1,15 @@
+﻿using GiftingApi.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace GiftingApi.Adapters;
+
+public class GiftingDataContext : DbContext
+{
+
+    public GiftingDataContext(DbContextOptions<GiftingDataContext> options) : base(options)
+    {
+
+    }
+    public DbSet<PersonEntity> People { get; set; } = null!;
+
+}
